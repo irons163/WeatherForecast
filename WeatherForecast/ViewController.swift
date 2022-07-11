b524c09a552040d058580e543156be7fb41e27cd
Phil Chang <phil.chang@yahooinc.com>
2022-07-11 18:08:16 +0800

    Initial Commit

---
+++ WeatherForecast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherForecast
+//
+//  Created by Phil Chang on 2022/7/11.
+//  Copyright © 2022 Yahoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
